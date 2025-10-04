2f04fced7a23da3a325cf2db0e1b848030e94e5d
levimarkmuigai <levimarkmuigai@gmail.com>
2025-10-04 22:17:45 +0300

    Initialize rustlink project with dependencies and basic structure
    - Created Cargo.toml with dependencies for axum, tokio, sqlx, dotenvy, tracing, and askama.
    - Added migration script to create links table with UUID as primary key.
    - Set up initial file structure for application commands, domain models, ports, database repository, web handlers, and routes.
    - Implemented a basic main function to start the application.

---
+++ migrations/20251001135214_create_links_table.sql
@@ -0,0 +1,10 @@
+-- Add migration script here
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+CREATE TABLE links (
+    -- Change BIGSERIAL to UUID and use a generation function
+    id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
+    short_code VARCHAR(10) NOT NULL UNIQUE,
+    long_url TEXT NOT NULL,
+    created_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+    delete_key_hash TEXT NOT NULL
+);
