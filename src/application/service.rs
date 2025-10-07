e23021941460e82702200d6fbdabbb598d513f29
levimarkmuigai <levimarkmuigai@gmail.com>
2025-10-07 21:52:43 +0300

    Implement LinkService structure and create_new_link method for link creation

--- src/application/service.rs
+++ src/application/service.rs
@@ -1,1 +1,21 @@
+use crate::domain::errors::LinkError;
+use crate::domain::link::Link;
+use crate::domain::ports::LinkRepository;
 
+use super::command::{CreateLinkCommand, LinkCreationResult};
+
+pub struct LinkService<R: LinkRepository> {
+    repo: R,
+}
+
+impl<R: LinkRepository> LinkService<R> {
+    pub fn new(repo: R) -> Self {
+        LinkService { repo }
+    }
+
+    pub async fn create_new_link(
+        &self,
+        command: CreateLinkCommand,
+    ) -> Result<LinkCreationResult, LinkError> {
+    }
+}
