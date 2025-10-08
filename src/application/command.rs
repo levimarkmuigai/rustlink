7989b49898666e303affb9edbe257f66904ce3bf
levimarkmuigai <levimarkmuigai@gmail.com>
2025-10-08 16:50:56 +0300

    Refactor project structure and prepare files for future implementation

--- src/application/command.rs
+++ src/application/command.rs
@@ -1,15 +1,1 @@
-#[derive(Debug, Clone)]
-pub struct CreateLinkCommand {
-    pub long_url: String,
 
-    // User provided secret used to manage link (A02)
-    pub delete_key: String,
-}
-
-#[derive(Debug, Clone)]
-pub struct LinkCreationResult {
-    pub short_code: String,
-
-    // User provided secret used to manage short code (A02)
-    pub delete_key: String,
-}
