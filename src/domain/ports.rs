536be22448c9ad87d755ec1e48a1a873d3c7fb82
levimarkmuigai <levimarkmuigai@gmail.com>
2025-10-14 09:41:27 +0300

    feat(LinkPersistence: delete_by_id,  save. LinkQuery: find_by_id, find_by_short_code)

--- src/domain/ports.rs
+++ src/domain/ports.rs
@@ -1,1 +1,16 @@
+use crate::domain::{
+    errors::LinkError,
+    link::{Link, LinkId},
+};
 
+pub trait LinkPersistence {
+    fn delete_by_id(&mut self, id: LinkId) -> Result<Option<Link>, LinkError>;
+
+    fn save(&mut self, link: Link) -> Result<Link, LinkError>;
+}
+
+pub trait LinkQuery {
+    fn find_by_id(&self, id: LinkId) -> Result<Link, LinkError>;
+
+    fn find_by_short_code(&self, short_code: &str) -> Result<Link, LinkError>;
+}
